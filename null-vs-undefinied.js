a9b9e339917c880a9321b037f69fa0b8c5c13a0f
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2021-05-03 17:50:44 +0600

    null vs undefined

---
+++ null-vs-undefinied.js
@@ -0,0 +1,28 @@
+let pakhi;
+console.log(pakhi);
+
+
+function add (num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+const result = add (13, 8);
+console.log(result);
+
+
+function add (num1, num2){
+        console.log(num1 , num2); 
+    }
+const result = add (13);
+console.log(result);
+
+
+const premik = {name: 'smart dude', phone:45673};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log (fun);
+
+
+let ages = [3, 6, 8];
+console.log(ages[11]);
