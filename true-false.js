7fbb3fe1fd44e3b211a305bee1c554069b2dd862
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2021-05-03 17:24:58 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,49 @@
+// let name = null;
+// console.name(name);
+// if (name){
+//     console.log('condition is true');
+// }
+// else{
+//     console.log('condition is false');
+// }
+
+
+// let name = [];
+// if (name){
+//     console.log('condition is true');
+// }
+// else{
+//     console.log('condition is false');
+// }
+
+
+// let name = false;
+// if (name){
+//     console.log('condition is true');
+// }
+// else{
+//     console.log('condition is false');
+// }
+
+
+let name = 0;
+if (name || name == 0){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+// aikhetre kintu truth hobe cz condition e oivabe set kore deya hoice
+
+
+
+// Falsy -
+// 0
+// ''
+// undefined
+// null
+// NaN 
+// false
+
+// Truthy -
+// '0'   ' '  []  {}  'false'
