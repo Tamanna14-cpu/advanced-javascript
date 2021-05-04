18a0bfd69526376550a7ff3e73b026cbdb03965c
Tamanna14-cpu <bimurto.bithy14@gmail.com>
2021-05-04 15:53:54 +0600

    double vs triple equal idea

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,55 @@
+// const first = 2;
+// const second = '2';
+// if (first === second){
+//     console.log('condition is true');
+// }
+// else {
+//     console.log('condition is false');
+// }
+
+
+// const first = 2;
+// const second = '2';
+// if (first == second){
+//     console.log('condition is true');
+// }
+// else {
+//     console.log('condition is false');
+// }
+
+
+const first = 1;
+const second = true;
+if (first == second){
+    console.log('condition is true');
+}
+else {
+    console.log('condition is false');
+}
+// js a 1 is equal to true so true dekhabe
+
+
+const first = 2;
+const second = true;
+if (first == second){
+    console.log('condition is true');
+}
+else {
+    console.log('condition is false');
+}
+// 2 or other number is not equal to true so false dekhabe
+
+
+
+const first = 0;
+const second = false;
+if (first == second){
+    console.log('condition is true');
+}
+else {
+    console.log('condition is false');
+}
+// js a 0 is equal to false so true dekhabe
+
+
+// last 3ta example === er khetre kaj kore na
